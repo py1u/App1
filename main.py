2ee8fa425dd4616370c3f58bdd215a56307f39e1
py1u <peter.lu723@gmail.com>
2024-02-20 01:08:04 -0800

    adjusted userRegister function to return registry; added incorrect password edge case

--- main.py
+++ main.py
@@ -2,37 +2,55 @@
 #
 
 import time
+
+userRegistry = []
+passKey = "novaboba"
+username = ";lkadjf;akdfj;akfj"
+password = "0-aifa;dfjadfja;lfj"
+numtrys = 3
+
+
 def Todo(prompt):
     print("choose a action: 1,2,3")
     print("1.Add: add a task to your list")
     print("2.Show: display all task")
     print("3.Edit: edit a task in your list")
     
+def giveRegisterCount(userRegistry):
+    
+    count = userRegistry.len()
+    return count
     
     
+def registerUser(userRegistry):
+    prompt_userName = input("what would you like your username to be? ")
+    userRegistry = userRegistry.append(prompt_userName)
+    print("Successfully Registered")
+    return userRegistry
     
-#input will be a task in a string format
 
-userKey, passKey = "bobanova", "novaboba"
-userRegistry = ["bobanova"]
+while username not in userRegistry:
+    #check username is in registry
+    username = input("Enter username:")
 
-username = input("Enter username:")
-
-#check username is in registry
-
-if username in userRegistry:
-    
-    #go onto password    
-    while password != passKey: 
-        
-        password = input("Enter password:")
-        if password == passKey:
+    if username in userRegistry:
+        #go onto password    
+        while password != passKey: 
             
-            print("access granted..")
-            break
-else:
-    print("user has not been registered.")
-    
+            password = input("Enter password:")
+            if password == passKey: 
+                print("access granted..")
+                break
+            else: print(f"access denied.\n you have {numtrys} left.")
+    else:
+        print("user has not been registered.")
+        reg_choice = input("would you like to add a user to the registry?(y/n) ")
+        if reg_choice == 'y':
+            registerUser(userRegistry)
+            
+        elif reg_choice != 'y':
+            print("bye")
+        
 
 
 
