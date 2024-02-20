8c6de84ca058cdc791935dc8432576a298429ca5
py1u <peter.lu723@gmail.com>
2024-02-20 00:28:11 -0800

    added user register to password prompt and validation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # App1 is a Todo list App
 #
 
+import time
 def Todo(prompt):
     print("choose a action: 1,2,3")
     print("1.Add: add a task to your list")
@@ -12,12 +13,26 @@ def Todo(prompt):
     
 #input will be a task in a string format
 
+userKey, passKey = "bobanova", "novaboba"
+userRegistry = ["bobanova"]
 
 username = input("Enter username:")
-password = input("Enter username:")
-password = "bobanova"
 
-while password != password
-if (password == prompt):
-    print("Access granted..")
-    print("Loading List for: ")
+#check username is in registry
+
+if username in userRegistry:
+    
+    #go onto password    
+    while password != passKey: 
+        
+        password = input("Enter password:")
+        if password == passKey:
+            
+            print("access granted..")
+            break
+else:
+    print("user has not been registered.")
+    
+
+
+
