fcea7d38710c0df40f8d1b35ecccd1e5219e1bf7
py1u <peter.lu723@gmail.com>
2024-02-23 00:32:55 -0800

    added while loop to repeat add, show, edit with new quit input case

--- main.py
+++ main.py
@@ -5,86 +5,93 @@ def doTodo():
     print("1.\"add\": add a task to your list")
     print("2.\"show\": display all tasks")
     print("3.\"edit\": edit a task in your list")
+    print("or quit")
     
     
-    userAction = input("choose a action: ")
-    match userAction:
-        
-        case "add":
-            todo = input("enter a task: ")
-            TodoList.append(todo)
-            print("added successfully:")
-        
-        case "show":
-            print("showing your Todo List:")
-            print(TodoList)
+    userAction = input("choose a action: ")    
+    while userAction != "quit":
+        match userAction:
             
-        
-        case "edit":
-            index = int(input("what todo would you like to change?"))
-            new_todo = input("what do you want to change it to?")
-            TodoList[index] = new_todo
-            print("editted sucessfully")
-
-    
-    
-    
-def giveRegisterCount(userRegistry):
-    
-    count = userRegistry.len()
-    return count
-    
-    
-def registerUser(userRegistry):
-    prompt_userName = input("what would you like your username to be? ")
-    userRegistry = userRegistry.append(prompt_userName)
-    print("Successfully Registered")
-    return userRegistry
-    
-
-def todoAuth():
-    #creation during auth
-    userRegistry = []
-    username = ''
-    numtrys = 3
-    userAuth = False;
-    passAuth = False;
-    passKey = "bobanova"
-    
-    while userAuth == False:
-        
-        username = input("Enter username:")
-
-        if username in userRegistry:
-            userAuth = True
+            case "add":
+                todo = input("enter a task: ")
+                TodoList.append(todo)
+                print("added successfully")
             
-            while passAuth == False: 
+            case "show":
+                print("showing your Todo List:")
+                print(TodoList)
                 
-                password = input("PassKey:")
-                if password == passKey: 
-                    print("access granted..")
-                    doTodo()
-                    passAuth = True
-                elif password != passKey: 
+            
+            case "edit":
+                index = int(input("what todo would you like to change?"))
+                new_todo = input("what do you want to change it to?")
+                TodoList[index] = new_todo
+                print("editted sucessfully")
+            
+            case "quit":
+                print("exiting todo list..")
+                break
+
+    
+    
+    
+# def giveRegisterCount(userRegistry):
+    
+#     count = userRegistry.len()
+#     return count
+    
+    
+# def registerUser(userRegistry):
+#     prompt_userName = input("what would you like your username to be? ")
+#     userRegistry = userRegistry.append(prompt_userName)
+#     print("Successfully Registered")
+#     return userRegistry
+    
+
+# def todoAuth():
+#     #creation during auth
+#     userRegistry = []
+#     username = ''
+#     numtrys = 3
+#     userAuth = False;
+#     passAuth = False;
+#     passKey = "bobanova"
+    
+#     while userAuth == False:
+        
+#         username = input("Enter username:")
+
+#         if username in userRegistry:
+#             userAuth = True
+            
+#             while passAuth == False: 
+                
+#                 password = input("PassKey:")
+#                 if password == passKey: 
+#                     print("access granted..")
+#                     doTodo()
+#                     passAuth = True
+#                 elif password != passKey: 
                     
-                    if numtrys == 0:
-                        print("maximum attempts reached. access locked")
-                        quit()
+#                     if numtrys == 0:
+#                         print("maximum attempts reached. access locked")
+#                         quit()
                         
-                    print(f"access denied.\n you have {numtrys} left.")
-                    numtrys -= 1
+#                     print(f"access denied.\n you have {numtrys} left.")
+#                     numtrys -= 1
                     
                     
-        else:
-            print("user has not been registered.")
-            reg_choice = input("would you like to add a user to the registry?(y/n) ")
-            if reg_choice == 'y':
-                registerUser(userRegistry)
+        # else:
+        #     print("user has not been registered.")
+        #     reg_choice = input("would you like to add a user to the registry?(y/n) ")
+        #     if reg_choice == 'y':
+        #         registerUser(userRegistry)
             
             
 #call auth for todo app start
 print("Starting App..\n")
-todoAuth()
+doTodo()
+#todoAuth()
 
 
 
