08d022dd41a9f798ce05dadea2a71fc7430962df
py1u <peter.lu723@gmail.com>
2024-02-29 00:27:44 -0800

    added enum index to show feature

--- src/App.py
+++ src/App.py
@@ -8,3 +8,16 @@ while True:
         case 'add':
                 todo = input("Enter a todo: ")
                 todos.append(todo)
+                
+        case 'show':
+            for index, item in enumerate(todos):
+                    print(index,'-', item)
+        
+        case 'edit':
+            number = int(input("Number of todo to edit:"))
+            number = number - 1
+            new_todo = input("Enter new todo:")
+            todos[number] = new_todo
+        
+        case 'quit':
+            break
