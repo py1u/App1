f950ee58f86663b2eb7f652f7a49e0bd2362f95b
py1u <peter.lu723@gmail.com>
2024-03-15 00:51:39 -0700

    added capitalize method to first document in list

--- bonus/bonus3.py
+++ bonus/bonus3.py
@@ -1,4 +1,6 @@
 filenames = ['document', 'report', 'presentation']
 
-for i,j in enumerate(filenames):
-    print(f"{i}-{j}.txt")
+# for i,j in enumerate(filenames):
+#     print(f"{i}-{j}.txt")
+
+print(filenames[0].upper())
