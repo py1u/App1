73101663fd5a01361de4c258876482c7f09c3935
py1u <peter.lu723@gmail.com>
2024-03-15 01:16:05 -0700

    temp commented out python file

--- bonus/bonus3.py
+++ bonus/bonus3.py
@@ -1,6 +1,18 @@
-# filenames = ['document', 'report', 'presentation']
+# # filenames = ['document', 'report', 'presentation']
 
-# for i,j in enumerate(filenames):
-#     print(f"{i}-{j.capitalize()}.txt")
+# # for i,j in enumerate(filenames):
+# #     print(f"{i}-{j.capitalize()}.txt")
 
-ips = ['100.122.133.105', '100.122.133.111']
+# ips = ['100.122.133.105', '100.122.133.111']
+
+# user_choice= input("Enter a value 0 or 1:")
+
+# match user_choice:
+    
+#     case '0':
+#         print("You chose", ips[0])
+#     case '1':
+#         print("You chose", ips[1])
+
+
+
