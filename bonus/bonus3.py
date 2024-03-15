e8eb60283b1c7f5d9a378a13120449044a326f2a
py1u <peter.lu723@gmail.com>
2024-03-15 00:59:00 -0700

    added two ip dummy addresses to ips list

--- bonus/bonus3.py
+++ bonus/bonus3.py
@@ -1,5 +1,6 @@
-filenames = ['document', 'report', 'presentation']
+# filenames = ['document', 'report', 'presentation']
 
-for i,j in enumerate(filenames):
-    print(f"{i}-{j.capitalize()}.txt")
+# for i,j in enumerate(filenames):
+#     print(f"{i}-{j.capitalize()}.txt")
 
+ips = ['100.122.133.105', '100.122.133.111']
