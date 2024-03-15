214bdf312237cca7bf3bf3fce871ec5975ae5c44
py1u <peter.lu723@gmail.com>
2024-03-15 00:28:48 -0700

    added sort method to wait_list

--- bonus/bonus3.py
+++ bonus/bonus3.py
@@ -1,6 +1,9 @@
-waiting_list = ["sen", "ben", "john"]
+waiting_list = ["Cen", "Aen", "Bohn"]
+
+sorted_list = waiting_list.sort()
 
 for index, item in enumerate(waiting_list):
+    
     row = f"{index + 1}.{item.capitalize()}"
     print(row)
     
