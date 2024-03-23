ed82ed01938f60d36c0a128b932b660fc535b6ed
Peter Lu <peter.lu723@gmail.com>
2024-03-23 16:55:23 -0700

    test case with three index variables in a tuple

--- experiments/e1.py
+++ experiments/e1.py
@@ -1,30 +1,3 @@
-todos = []
-
-while True:
-    user_action = input("Type add, show,edit, complete or exit:")
-    user_action = user_action.strip()
-    
-    match user_action:
-        case 'add':
-                todo = input("Enter a todo: ")
-                todos.append(todo)
-                
-        case 'show':
-            for index, item in enumerate(todos):
-                    row= f"{index + 1}.{item}"
-                    print(row)
-                  
-            
-        
-        case 'edit':
-            number = int(input("Number of todo to edit:"))
-            number = number - 1
-            new_todo = input("Enter new todo:")
-            todos[number] = new_todo
-        
-        case 'complete':
-            number = int(input("Number of todo to complete:"))
-            todos.pop(number - 1)
-    
-        case 'exit':
-            break
+buttons = [('John', 'Sen', 'Morro'), ('Lin', 'Ajay', 'Filip')]
+for first, second, third in buttons:
+    print(first, second, third)
