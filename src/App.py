7a7bbaad1144684d202cb4ab9e35fef8e10e65fa
Peter Lu <peter.lu723@gmail.com>
2024-03-23 17:40:56 -0700

    added file write to todos.txt feature

--- src/App.py
+++ src/App.py
@@ -6,23 +6,21 @@ while True:
     
     match user_action:
         case 'add':
-                todo = input("Enter a todo: ")
-                todos.append(todo)
-                
+            todo = input("Enter a todo: ") + "\n"
+            todos.append(todo)
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
         case 'show':
             for index, item in enumerate(todos):
-                    row= f"{index + 1}.{item}"
-                    print(row)
-        
+                row = f"{index + 1}.{item}"
+                print(row)
         case 'edit':
             number = int(input("Number of todo to edit:"))
             number = number - 1
             new_todo = input("Enter new todo:")
             todos[number] = new_todo
-        
         case 'complete':
             number = int(input("Number of todo to complete:"))
             todos.pop(number - 1)
-    
         case 'quit':
             break
