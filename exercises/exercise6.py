9a08994347f840d77cafbb3a4ef88c7fe4a5cfbe
Peter Lu <peter.lu723@gmail.com>
2024-03-24 22:56:43 -0700

    implementation fixed

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -1,6 +1,6 @@
 filenames = ['doc.txt', 'report.txt', 'presentation.txt']
 
 for filename in filenames:
-    file = open(f'../files/{filename}'append, 'w')
+    file = open(f'../files/{filename}', 'w')
     file.write('Hello')
     file.close()
