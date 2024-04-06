8cc89d510ba6d64807f7fabc73ce1c049f1fe4aa
Peter Lu <peter.lu723@gmail.com>
2024-04-06 13:40:32 -0700

    updated exercise 11

--- exercises/exercise11.py
+++ exercises/exercise11.py
@@ -1,9 +1,17 @@
-try:
-    total_value = float(input("Enter total value: "))
-    value = float(input("Enter value:"))
+# try:
+#     total_value = float(input("Enter total value: "))
+#     value = float(input("Enter value:"))
+#
+#     percent = value/total_value * 100
+#     print(f"That is {percent}%")
+#
+# except ZeroDivisionError:
+#     print("Your total value cannot be zero.")
 
-    percent = value/total_value * 100
-    print(f"That is {percent}%")
+colors = [11, 34, 98, 43, 45, 54, 54]
 
-except ZeroDivisionError:
-    print("Your total value cannot be zero.")
+for color in colors:
+    if color > 50:
+        print(color)
+    else:
+        continue
