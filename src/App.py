22916fd23e9ee92851ca7cc1e60e5755b25de8f1
Peter Lu <peter.lu723@gmail.com>
2024-04-07 11:45:36 -0700

    added filename argument to get_todos function

--- src/App.py
+++ src/App.py
@@ -1,7 +1,8 @@
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
+
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
@@ -10,7 +11,7 @@ while True:
     if user_action.startswith('add') or user_action.startswith('new'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         todos.append(todo + '\n')
 
@@ -18,7 +19,7 @@ while True:
             file.writelines(todos)
     elif user_action.startswith('show'):
 
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -30,7 +31,7 @@ while True:
             print(f"edit #{number}")
             number = number - 1
 
-            get_todos()
+            todos = get_todos('todos.txt')
 
             new_todo = input("Enter new todo:")
             todos[number] = new_todo + '\n'
@@ -46,7 +47,7 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = get_todos('todos.txt')
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
