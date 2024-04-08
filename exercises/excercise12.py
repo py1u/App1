4c8114fef300a59b5791a28c2bc1ec08118a311d
Peter Lu <peter.lu723@gmail.com>
2024-04-07 23:17:49 -0700

    added password validation function

--- exercises/excercise12.py
+++ exercises/excercise12.py
@@ -4,5 +4,33 @@ def liters_to_m3(liters):
     return cubic
 
 
+# 2. password checker from bonus 9
 
+def strength(password):
+    result = {}
+
+    if len(password) >= 8:
+        result["length"] = True
+    else:
+        result["length"] = False
+
+    digit = False
+    for i in password:
+        if i.isdigit():
+            digit = True
+    result["digits"] = digit
+
+    uppercase = False
+    for i in password:
+        if i.isupper():
+            uppercase = True
+    result["uppercase"] = uppercase
+
+    print(result.values())
+    if all(result.values()):
+        return "Strong Password"
+    else:
+        return "Weak Password"
+
+# 3
 
