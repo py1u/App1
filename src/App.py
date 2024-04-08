4c1d6d58be06bd12de27a00f262ff7069b4bed76
Peter Lu <peter.lu723@gmail.com>
2024-04-07 23:00:16 -0700

    updated App

--- src/App.py
+++ src/App.py
@@ -3,6 +3,7 @@ def get_todos(filepath):
         todos_local = file_local.readlines()
     return todos_local
 
+
 def write_todos(filepath, todos_arg):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
