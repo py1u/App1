675617a8d43a55b2839f29c6ab6644d42b8b9e5e
Peter Lu <peter.lu723@gmail.com>
2024-04-09 00:53:25 -0700

    docstring for get_todos

--- src/App.py
+++ src/App.py
@@ -1,4 +1,7 @@
 def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of
+    to-do items.
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
